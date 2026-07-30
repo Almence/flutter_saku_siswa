15ef32ca571b0cba496a6b3294508f3782a61919
Almence <albariridwan88@gmail.com>
2026-07-30 11:28:17 +0700

    Fix import statements in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:sakuSiswaApp/views/dashboard_screen.dart';
+import 'package:saku_siswa/views/dashboard_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
